ab8ce7f247be0856f70a512fa8e45d2e2ecec8db
Luka288 <75569044+Luka288@users.noreply.github.com>
2024-06-17 23:18:00 +0400

    responsive, preparing for auth

---
+++ src/app/components/shared/services/auth-service.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { authEverset } from '../consts';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+  private readonly http = inject(HttpClient);
+
+  readonly authUrl = authEverset;
+
+
+  register(){
+
+  }
+
+  login(){
+
+  }
+
+}
