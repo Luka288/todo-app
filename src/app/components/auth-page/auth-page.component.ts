0a4bdd6c55ec540fed7d726b776a92ba6ac75e7a
Luka288 <75569044+Luka288@users.noreply.github.com>
2024-06-19 10:54:45 +0400

    authpage

--- src/app/components/auth-page/auth-page.component.ts
+++ src/app/components/auth-page/auth-page.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-auth-page',
   standalone: true,
-  imports: [RouterModule, RouterOutlet],
+  imports: [RouterModule, RouterOutlet, MatInputModule, CommonModule, ReactiveFormsModule],
   templateUrl: './auth-page.component.html',
-  styleUrl: './auth-page.component.scss'
+  styleUrl: './auth-page.component.scss',  
 })
 export default class AuthPageComponent {
+  private readonly fb = inject(FormBuilder)
 
+
+  readonly signUpForm = this.fb.group({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(16),
+      Validators.minLength(2),
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(16),
+      Validators.minLength(2)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ])
+  })
 }
