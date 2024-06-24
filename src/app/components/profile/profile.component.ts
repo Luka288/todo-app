214fecb5058d671909cb674547e2891f40c5259c
Luka288 <75569044+Luka288@users.noreply.github.com>
2024-06-24 22:07:50 +0400

    Profile page
    displaying Email, first name, last name, profile picture on profile page

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { AuthServiceService } from '../shared/services/auth-service.service';
+import { Observable } from 'rxjs';
+import { user } from '../shared';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule, CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export default class ProfileComponent {
+  user$: Observable<user | null>;
 
+  constructor(private authService: AuthServiceService) {
+    this.user$ = this.authService.user$;
+  }
 }
